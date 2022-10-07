4f10008729b12ca2b3bd869089b00b5d1ee47976
Manoj Kumar A <QU8T@eurofins.com>
2022-10-07 13:32:44 +0530

    latest

--- core/settings.py
+++ core/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-oazjrmep^zvmo(=8v32aj6dtksue3%4xcft(@^ipb&f30r%$1b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
